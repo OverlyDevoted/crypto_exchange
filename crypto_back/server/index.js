1b8897987d9dd24e48af379e393dfdcb26bac6b8
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2023-08-31 01:27:35 +0300

    Finished backend

--- crypto_back/server/index.js
+++ crypto_back/server/index.js
@@ -3,26 +3,60 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-if(!process.env.MONGODB_URI)
-{
+if (!process.env.MONGODB_URI) {
     console.error("Could not find MongoDB deployment URI. Closing server...")
     return;
 }
 const dbHandler = require("./db.js");
 const db = new dbHandler(process.env.MONGODB_URI);
-db.connect();
-
-
 
 const express = require("express");
 //Backend server code, api 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.get('/api', (req, res) => {
-    console.log("User searched for: " + req.query.search);
-    // res.json({ message: "Hello from server!" });
+app.get('/api/search', (req, res) => {
+    const cryptocurrency = req.query.value; 
+    const user = req.query.user;
+    if (!cryptocurrency || !user) {
+        console.log("No parameter \'value\' or \'user\' was provided");
+        res.json({ message: `No parameter \'value\' or \'user\' was provided`, response: 502 });
+        return;
+    }
+    console.log(`User ${user} searched for: ${cryptocurrency}`);
+    db.insertSearch("cryptoExchange", "search", {cryptocurrency: cryptocurrency, user: user});
+
+    res.json({ message: `User ${user} successfully inserted search value: ${cryptocurrency}`, response: 200 });
 })
+
+app.get('/api/find', (req, res) => {
+    const cryptocurrency = req.query.value;
+    const user = req.query.user;
+    if (!cryptocurrency || !user) {
+        console.log("No parameter \'value\' or \'user\' was provided");
+        res.json({ message: `No parameter \'value\' or \'user\' was provided`, response: 502 });
+        return;
+    }
+    db.find("cryptoExchange", "search", { cryptocurrency: cryptocurrency, user: user });
+    console.log("finding");
+    res.json({ message: "finding" });
+})
+/* 
+app.get('/api/select', (req, res) => {
+    const search = req.query.value;
+    const user = req.query.user;
+    if (!search || !user)
+    {
+        console.log("No parameter \'search\' was provided");
+        res.json({ message: `Server could not resolve the request`, response: 502 });
+        return;
+    }
+    
+    console.log("User searched for: " + search);
+    db.insertSingle("cryptoExchange", user, { selection: search, date: Date() });
+    res.json({ message: `Successfully inserted search value: ${search}`, response: 200 });
+}) */
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
